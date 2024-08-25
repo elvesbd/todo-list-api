70830e9fb22a5a132eb75c305886e604136d9a92
elvesbd <elvesbd@yahoo.com>
2024-08-25 16:51:40 -0300

    chore: applying white list

--- src/config/setup-app.ts
+++ src/config/setup-app.ts
@@ -1,6 +1,13 @@
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 
 export const setupApp = (app: INestApplication): INestApplication => {
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   app.enableCors({
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
